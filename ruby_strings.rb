14bb4d3bf6a562bdfd700d5ac957cbf894948aea
rebeccaeli20 <rebeccaeli20@gmail.com>
2018-07-10 16:48:51 +0000

    automatically backed up by learn

---
+++ ruby_strings.rb
@@ -0,0 +1,21 @@
+# Ruby Strings lab 
+
+
+
+print "Dear "
+
+puts "Hermione,"
+
+print "You are cordially invited to the"
+
+puts "Best Halloween Party Ever on "
+
+puts "October 31"
+
+print "at "
+
+puts "6 pm."
+
+
+
+puts "Harry Potter"
